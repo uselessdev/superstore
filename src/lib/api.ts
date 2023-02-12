ac7683e7d76cb21f73d39b2f40fb841d56892a89
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-02-12 03:42:17 -0300

    feat(api): add settings for trpc

---
+++ src/lib/api.ts
@@ -0,0 +1,31 @@
+import { httpBatchLink, loggerLink } from '@trpc/client'
+import { createTRPCNext } from '@trpc/next'
+import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
+import superjson from 'superjson'
+
+import { type AppRouter } from '@/server/api/root'
+import { getBaseUrl } from '@/functions/get-base-url'
+
+export const api = createTRPCNext<AppRouter>({
+  config() {
+    return {
+      transformer: superjson,
+
+      links: [
+        loggerLink({
+          enabled: opts =>
+            process.env.NODE_ENV === 'development' ||
+            (opts.direction === 'down' && opts.result instanceof Error),
+        }),
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+
+      ssr: false,
+    }
+  },
+})
+
+export type RouterInputs = inferRouterInputs<AppRouter>
+export type RouterOutputs = inferRouterOutputs<AppRouter>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppType } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import type { Session } from 'next-auth'
+import { api } from '@/lib/api'
 
 const App: AppType<{ session: Session | null }> = ({
   Component,
@@ -14,4 +15,4 @@ const App: AppType<{ session: Session | null }> = ({
   )
 }
 
-export default App
+export default api.withTRPC(App)
