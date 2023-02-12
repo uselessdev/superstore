09ddd302bef863352d7f37957a3cb85239f0ad13
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-02-12 05:45:03 -0300

    feat(companies): add create company event

---
+++ src/events.ts
@@ -0,0 +1,3 @@
+export enum Events {
+  CREATE_COMPANY = 'CREATE_COMPANY',
+}
--- src/server/api/routers/companies.ts
+++ src/server/api/routers/companies.ts
@@ -1,6 +1,7 @@
 import { TRPCError } from '@trpc/server'
 import { z } from 'zod'
 import * as CNPJ from '@fnando/cnpj'
+import { Events } from '@/events'
 import { createTRPCRouter, protectedProcdedure } from '../trpc'
 
 export const companiesRouter = createTRPCRouter({
@@ -22,13 +23,26 @@ export const companiesRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id
+
       try {
         const company = await ctx.prisma.company.create({
           data: input,
         })
 
+        if (company) {
+          await ctx.prisma.event.create({
+            data: {
+              userId,
+              event: Events.CREATE_COMPANY,
+              payload: JSON.stringify(input),
+            },
+          })
+        }
+
         return company
       } catch (error) {
+        console.log(error)
         throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR' })
       }
     }),
